25aed69a3ccceba609cecfad84cb9f37687260ff
soirihiroka <133444309+soirihiroka@users.noreply.github.com>
2026-08-26 03:55:28 +0000

    Fix submodules

--- external/manim/manimlib/renderer/__init__.py
+++
@@ -1,4 +0,0 @@
-from manimlib.renderer.gpu import Gpu
-from manimlib.renderer.renderer import Renderer
-
-__all__ = ["Gpu", "Renderer"]
--- external/slang/external/slang-rhi/src/d3d12/d3d12-api.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include <windows.h>
-#include <d3d12.h>
-#include <dxgi1_4.h>
-#include <D3D12MemAlloc.h>
